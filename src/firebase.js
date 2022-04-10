c726f88d97642321e39d2283e2f3c41b33ed0d6e
Sanjheev R <54068975+sanjheev100@users.noreply.github.com>
2022-04-10 09:54:10 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/compat/auth'
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDMKw9JRK43viFsCIJT65tt3hSo9-DKByo',
-  authDomain: 'mercury-6b1ca.firebaseapp.com',
-  projectId: 'mercury-6b1ca',
-  storageBucket: 'mercury-6b1ca.appspot.com',
-  messagingSenderId: '893658495344',
-  appId: '1:893658495344:web:2c45e51c72600a7f6e37b5',
+    //YOUR FIREBASE CONFIG
 }
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig)
